73b31177bea0b4735dfd2de771384d31adb9fae4
Paulo André Cardoso Silva Santos <pauloandrecss@gmail.com>
2023-09-01 22:46:38 -0300

    added apps and rest framework to the settings

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
+    'order',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
